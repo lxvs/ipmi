8a0137fcd680c0df2b01ae594098c8e77434f078
lxvs <39054009+lxvs@users.noreply.github.com>
2021-01-31 19:14:04 +0800

    v2.0  2020.11.27
    1. IP address parsing function is stronger.
    2. Add custom SOL log filename support.
    3. Minor improvement on help information.
    4. Minor improvement on error prompt.

--- ipmi.bat
+++ ipmi.bat
@@ -1,15 +1,10 @@
 @echo off
 setlocal
 if "%1"=="" goto usage
-if /i "%1"=="t" if "%3"=="" ping -t 100.2.76.%2 & goto:eof
-if /i "%1"=="-t" if "%3"=="" ping -t 100.2.76.%2 & goto:eof
 set /a host=%1 2>NUL || goto hostparse
 if "%host%" NEQ "%1" ipmitool %* & goto:eof
-if "%2"=="" ping 100.2.76.%1 & goto:eof
-if /i "%2"=="t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
-if /i "%2"=="-t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
-if /i "%2"=="sol" if "%3"=="" goto SOL
-ipmitool -I lanplus -U admin -P admin -H 100.2.76.%* & goto:eof
+set hostpre=100.2.76.
+goto exec
 :hostparse
 set /a secnum=0
 for /f "delims=. tokens=1-4,*" %%a in ("%1") do (
@@ -21,27 +16,50 @@ for /f "delims=. tokens=1-4,*" %%a in ("%1") do (
 )
 :afterhostparse
 if defined sece ((call:err Fatal 230 "IP input has more than 4 sections.") & goto:eof)
+if defined secd (set hostpre=) & goto exec
+if defined secc (set hostpre=100.) & goto exec
+if defined secb (set hostpre=100.2.) & goto exec
+call:err Warning 240 "Script went to an unexpected place!"
+:exec
+if "%2"=="" ping %hostpre%%1 -n 2 & goto:eof
+if /i "%2"=="t" if "%3"=="" ping -t %hostpre%%1 & goto:eof
+if /i "%2"=="-t" if "%3"=="" ping -t %hostpre%%1 & goto:eof
+if /i "%2"=="sol" (
+    if "%3"=="" ((call:SOL %1) & goto:eof)
+    if "%4"=="" (
+        set solarg=%3
+        goto solargparse
+    )
+    goto default
+)
+goto default
+:solargparse
+if "%solarg:~-4%"==".log" ((call:SOL %1 %3) & goto:eof)
+if "%solarg:~-4%"==".txt" ((call:SOL %1 %3) & goto:eof)
+:default
+ipmitool -I lanplus -U admin -P admin -H %hostpre%%* & goto:eof
 :usage
 echo.
-echo. IPMI script 20201126
+echo. IPMI script 20201127
 echo. Johnny Appleseed ^<lllxvs+github.ipmi@gmail.com^>
 echo. 
 echo. Usage:
 echo.
-echo.   ipmi ^<last section of IP^> arg1 [arg2 [...]]   Send IPMITool commands
+echo.   ipmi ^<IP^> arg1 [arg2 [...]]               Send IPMITool commands
 call:LFN xxx fn
-echo.   ipmi ^<last section of IP^> SOL                 Collect SOL log to %fn%
-echo.   ipmi ^<last section of IP^> [t]                 Ping
-echo.   ipmi [arg1 [arg2 [...]]]                      Get ipmitool help on specific parameter(s).
+echo.   ipmi ^<IP^> SOL                             Collect SOL log to %fn%
+echo.   ipmi ^<IP^> SOL [^<FN^>.log ^| ^<FN^>.txt]       Collect SOL log to %cd%\^<FileName^>
+echo.   ipmi ^<IP^> [t]                             Ping
+echo.   ipmi [arg1 [arg2 [...]]]                  Get ipmitool help on specific parameter(s).
 echo.
 echo. Examples:
 echo.   ipmi 255 arg1 arg2 arg3
 echo.     stands for:
 echo.   ipmitool -I lanplus -U admin -P admin -H 100.2.76.255 arg1 arg2 arg3
 echo.
-echo.   ipmi t 255
+echo.   ipmi 254.255 t
 echo.     stands for:
-echo.   ping -t 100.2.76.255
+echo.   ping 100.2.254.255 -t
 echo.
 goto:eof
 
@@ -58,18 +76,20 @@ goto:eof
 echo.
 echo.^> Deactivating previous SOL...
 echo.
-ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol deactivate
-call:LFN %1 logfilename 1
+ipmitool -I lanplus -U admin -P admin -H %hostpre%%1 sol deactivate
+if "%2" NEQ "" set logfilename=%cd%\%2
+if not defined logfilename (call:LFN %1 logfilename 1)
 type nul> %logfilename% || ((call:err Fatal 510 "Cannot create log file, please consider to change a directory or run as administrator.") & goto:eof)
 echo.
 echo.^> Log file saved to %logfilename%
 echo.^> Activate SOL...
 echo.
 explorer /select,"%logfilename%"
-ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol activate >> %logfilename%
+ipmitool -I lanplus -U admin -P admin -H %hostpre%%1 sol activate >> %logfilename%
 goto:eof
 
 :err
 echo.
-echo.^> %1 error (%2): %~3
+echo.^> %1^(%2^): %~3
+echo.
 goto:eof
