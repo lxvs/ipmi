cafef51ea20384d1e541fb86cd7de8c4a2847cb0
lxvs <39054009+lxvs@users.noreply.github.com>
2021-01-31 18:37:08 +0800

    v1.0  2020.11.16
    1. Make SOL log collecting more convenient.
    2. Add ping -t support.

--- ipmi.bat
+++ ipmi.bat
@@ -1,16 +1,30 @@
 @echo off
-if "%1" EQU "" (goto usage)
-set /a host=%1
-if %host% NEQ %1 (goto usage)
-if "%2" EQU "" (
-    ping 100.2.76.%1
+if "%1"=="" goto usage
+set /a host=%1 >NUL 2>&1
+if "%host%" NEQ "%1" goto usage
+if "%2"=="" ping 100.2.76.%1 & goto:eof
+if "%2"=="t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
+if "%2"=="-t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
+if "%2"=="sol" (
+    SETLOCAL ENABLEDELAYEDEXPANSION
+    if "%date:~5,2%"=="10" set "month=A" & goto continue
+    if "%date:~5,2%"=="11" set "month=B" & goto continue
+    if "%date:~5,2%"=="12" set "month=C" & goto continue
+    set "month=%date:~6,1%"
+    :continue
+    if "%date:~8,1%"=="0" (set "day=%date:~9,1%") else set "day=%date:~8,2%"
+    if "%time:~0,1%"==" " (set "hm=%time:~1,1%%time:~3,1%") else set "hm=%time:~0,2%%time:~3,1%"
+    set "logfile=76.%1.!month!!day!.!hm!.log"
+    ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol deactivate >NUL 2>&1
+    ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol activate > !logfile!
+    SETLOCAL DISABLEDELAYEDEXPANSION
     goto:eof
 )
 ipmitool -I lanplus -U admin -P admin -H 100.2.76.%*
 goto:eof
 :usage
 echo.
-echo. IPMI script by Johnny Appleseed ^<lllxvs+github.ipmi@gmail.com^> Updated on 20201115
+echo. IPMI script by Johnny Appleseed ^<lllxvs+github.ipmi@gmail.com^> Updated on 20201116
 echo. 
 echo. Usage:
 echo.
