32415d0e5c03bb1e860367ec507d71fd36d05a4f
lxvs <39054009+lxvs@users.noreply.github.com>
2021-01-31 18:43:51 +0800

    v1.1  2020.11.20
    1. Pick the SOL function out of IF block to avoid enabling delayed expansion.
    2. Make log filename calculation be an independent function.
    3. Minor improvement on 'ping'.

--- ipmi.bat
+++ ipmi.bat
@@ -1,39 +1,48 @@
 @echo off
 if "%1"=="" goto usage
-set /a host=%1 >NUL 2>&1
+if /i "%1"=="t" if "%3"=="" ping -t 100.2.76.%2 & goto:eof
+if /i "%1"=="-t" if "%3"=="" ping -t 100.2.76.%2 & goto:eof
+set /a host=%1
 if "%host%" NEQ "%1" goto usage
 if "%2"=="" ping 100.2.76.%1 & goto:eof
-if "%2"=="t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
-if "%2"=="-t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
-if "%2"=="sol" (
-    SETLOCAL ENABLEDELAYEDEXPANSION
-    if "%date:~5,2%"=="10" set "month=A" & goto continue
-    if "%date:~5,2%"=="11" set "month=B" & goto continue
-    if "%date:~5,2%"=="12" set "month=C" & goto continue
-    set "month=%date:~6,1%"
-    :continue
-    if "%date:~8,1%"=="0" (set "day=%date:~9,1%") else set "day=%date:~8,2%"
-    if "%time:~0,1%"==" " (set "hm=%time:~1,1%%time:~3,1%") else set "hm=%time:~0,2%%time:~3,1%"
-    set "logfile=76.%1.!month!!day!.!hm!.log"
-    ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol deactivate >NUL 2>&1
-    ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol activate > !logfile!
-    SETLOCAL DISABLEDELAYEDEXPANSION
-    goto:eof
-)
-ipmitool -I lanplus -U admin -P admin -H 100.2.76.%*
-goto:eof
+if /i "%2"=="t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
+if /i "%2"=="-t" if "%3"=="" ping -t 100.2.76.%1 & goto:eof
+if /i "%2"=="sol" goto SOL
+ipmitool -I lanplus -U admin -P admin -H 100.2.76.%* & goto:eof
 :usage
 echo.
-echo. IPMI script by Johnny Appleseed ^<lllxvs+github.ipmi@gmail.com^> Updated on 20201116
+echo. IPMI script 20201120
+echo. Johnny Appleseed ^<lllxvs+github.ipmi@gmail.com^>
 echo. 
 echo. Usage:
 echo.
+echo.   ipmi ^<last section of IP^> [t]                         Ping
+echo.   ipmi ^<last section of IP^> [arg1 [arg2 [arg3 [...]]]   Send IPMITool commands
+call:LFN xxx fn
+echo.   ipmi ^<last section of IP^> SOL                         Collect SOL log to %cd%\%fn%
+echo.
+echo. Examples:
 echo.   ipmi 255 arg1 arg2 arg3
-echo.   stands for:
+echo.     stands for
 echo.   ipmitool -I lanplus -U admin -P admin -H 100.2.76.255 arg1 arg2 arg3
 echo.
 echo.   ipmi 255
-echo.   stands for:
+echo.     stands for:
 echo.   ping 100.2.76.255
 echo.
+goto:eof
+
+:LFN
+set "month=%date:~5,2%"
+if "%date:~8,1%"=="0" (set "day=%date:~9,1%") else set "day=%date:~8,2%"
+if "%time:~0,1%"==" " (set "hm=0%time:~1,1%%time:~3,2%") else set "hm=%time:~0,2%%time:~3,2%"
+set "%2=76.%1.%month%%day%.%hm%.log"
+goto:eof
+
+:SOL
+ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol deactivate >NUL 2>&1
+call:LFN %1 logfilename
+type nul> %logfilename%
+explorer /select,"%cd%\%logfilename%"
+ipmitool -I lanplus -U admin -P admin -H 100.2.76.%1 sol activate >> %logfilename%
 goto:eof
