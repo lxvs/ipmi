05ad194cc49f324b7784c3acf18491d73439822e
Johnny Appleseed <39054009+lxvs@users.noreply.github.com>
2021-03-02 16:59:59 +0800

    minor performance improvement

--- ipmi.bat
+++ ipmi.bat
@@ -311,12 +311,12 @@ goto:eof
 
 :gettime
 for /f "tokens=1-6 usebackq delims=_" %%a in (`powershell -command "&{Get-Date -format 'yyyy_MM_dd_HH_mm_ss'}"`) do (
-    if "%1" NEQ "" set %1=%%a
-    if "%2" NEQ "" set %2=%%b
-    if "%3" NEQ "" set %3=%%c
-    if "%4" NEQ "" set %4=%%d
-    if "%5" NEQ "" set %5=%%e
-    if "%6" NEQ "" set %6=%%f
+    if "%1" NEQ "" set "%1=%%a" else goto:eof
+    if "%2" NEQ "" set "%2=%%b" else goto:eof
+    if "%3" NEQ "" set "%3=%%c" else goto:eof
+    if "%4" NEQ "" set "%4=%%d" else goto:eof
+    if "%5" NEQ "" set "%5=%%e" else goto:eof
+    if "%6" NEQ "" set "%6=%%f" else goto:eof
 )
 goto:eof
 
